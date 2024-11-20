e9b601a9f384998efeac219dbafeb05fc48d9a92
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-11-20 19:49:29 +0530

    Schema Update

--- utils/schemas.py
+++ utils/schemas.py
@@ -1,12 +1,7 @@
-import os
 from pydantic import BaseModel
-from typing import Optional
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
 
 
 class MailRequest(BaseModel):
     mail_content: str
-    mail_tone: Optional[str] = "Clear and Appreciative"
-    model_name: Optional[str] = os.getenv('GEMINI_MODEL')
+    # mail_tone: Optional[str] = "Clear and Appreciative"
+    # model_name: Optional[str] = os.getenv('GEMINI_MODEL')
