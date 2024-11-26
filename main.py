5e5c0e21847f733bd4b4e75e46b9bf8d1364692c
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-11-26 07:33:16 +0000

    added cors

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from utils import (
     get_llm_response,
     MailRequest
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv(override=True)
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
@@ -18,6 +19,17 @@ logger = get_logger("Mail AI", "app.log")
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 router = APIRouter(
     prefix="/api/v1",
     responses={404: {"description": "Not found"}},
